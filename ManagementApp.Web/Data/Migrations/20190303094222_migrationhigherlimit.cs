a57b4a57b1800391b9381d0fba45e507f5671fc8
Piotr Przetak <przetakpiotr94@gmail.com>
2019-03-03 11:53:16 +0100

    cos zrobiłem

---
+++ ManagementApp.Web/Data/Migrations/20190303094222_migrationhigherlimit.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManagementApp.Web.Data.Migrations
+{
+    public partial class migrationhigherlimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Weather",
+                table: "Protocols",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Protocols",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Employees",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Employees",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                maxLength: 14,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 11,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Employees",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Clients",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Clients",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Clients",
+                maxLength: 14,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 11,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Clients",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Weather",
+                table: "Protocols",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Protocols",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Employees",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Employees",
+                maxLength: 5,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 6,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                maxLength: 11,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 14,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Employees",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Clients",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Clients",
+                maxLength: 5,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 6,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Clients",
+                maxLength: 11,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 14,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Clients",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "City", "Email", "FirstName", "IsOccupied", "LastName", "PhoneNumber", "PostalCode", "Street" },
+                values: new object[] { 1, null, null, "Andrzej", false, "Duda", null, null, null });
+        }
+    }
+}
